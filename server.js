3cf7b8db3ada392734d5379155a9b6e44d0a6bf7
Trevor Oakley <sales@merrows.co.uk>
2018-10-01 10:50:31 +0000

    server

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const mongoose = require('mongoose');
 const solc = require('solc');
 let Web3 = require('web3');
 
+let sgMail = require('@sendgrid/mail');
+
 let web3 = new Web3();
 
 
@@ -47,10 +49,18 @@ var codeSchema = new mongoose.Schema({
   Hash : { type: String, default: null, uniq: true }
 });
 
+var userSchema = new mongoose.Schema({
+  UserName : { type: String, default: null },
+  EmailAddress : { type: String, default: null},
+  Password : {type: String, default: null},
+  Employee: {type: Boolean, default: null},
+  Employer: {type: Boolean, default: null}
+});
+
 var codeDB = mongoose.model("codes", codeSchema);
 var walletDB = mongoose.model("wallets", walletSchema);
 var bonusnameDB = mongoose.model("bonusnames", bonusnameSchema);
-
+var usersDB = mongoose.model("users", userSchema);
 
 app.post("/api/save_code", function(req, res){
   var data = req.body.data;
@@ -133,6 +143,85 @@ app.post("/api/save_bonusname", function(req, res){
   });
 });
 
+app.post("/api/reguser", function(req, res){
+  var username = req.body.username;
+  var emailaddress = req.body.emailaddress;
+  var password = req.body.password;
+  var employee = req.body.employee;
+  var employer = req.body.employer;
+
+  var userCreate = new usersDB({
+    UserName : username,
+    EmailAddress : emailaddress,
+    Password : password,
+    Employee : employee,
+    Employer : employer
+  });
+
+  usersDB.findOne({ UserName : username}, function(err, doc){
+    if(doc == null){
+      userCreate.save(function(err, doc){
+        if(err) throw err;
+
+         res.json({ message: "User Created", doc : doc, type: 'success'});
+      });
+    }else{
+         res.json({ message: "Already Exists", doc : doc, type: 'error'});
+    }
+  });
+});
+
+app.post("/api/loginuser", function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+
+  usersDB.findOne({ $and: [{ UserName : username}, {Password : password} ]}, function(err, doc){
+    if(doc == null){
+         res.json({ message: "Login details are incorrect", doc : doc, type: 'error'});
+    }else{
+         res.json({ message: "Correct", doc : doc, type: 'correct'});
+    }
+  });
+});
+
+
+
+app.post("/api/get_info", function(req, res){
+  var hash = req.body.hash;
+  codeDB.findOne({ Hash : hash}, function(err, doc){
+    if(err) res.json({ message: "Error", error: err, type: 'error' });
+
+     res.json({ message: "Displayed Code", doc : doc, type: 'success' });
+  });
+});
+
+
+
+
+
+app.post("/api/sendmail", function(req, res){
+
+  var emailfrom = req.body.emailfrom;
+  var emailto = req.body.emailto;
+  var subject = req.body.subject;
+  var text = req.body.text;
+
+var s = '<p>'+text+'</p>';
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    //  sgMail.setApiKey(key);#
+console.log(sgMail);
+      const msg = {
+      to: emailto,
+      from: emailfrom,
+      subject: subject,
+      text: text,
+      html: s,
+      };
+console.log(msg);
+      sgMail.send(msg);
+
+  });
+
 
 app.post("/api/update_code", function(req, res){
   var hash = req.body.hash;
@@ -201,6 +290,6 @@ app.post("/api/get_wallets", function(req, res){
 
 app.use(express.static(__dirname + "/public" ));
 
-app.listen(3000, function(){
-  console.log("App starts at port :" +3000);
+app.listen(80, function(){
+  console.log("App starts at port :" +80);
 });
